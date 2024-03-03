3bba3fd994feedaabf0ba692896439c696d652d7
EmmaArthur <e.arthur@amalitech.org>
2024-03-03 00:04:22 +0000

    Updates

---
+++ precache-manifest.56e417c321aaff40d604a95cddec4806.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "756edd65f8a468c1809c7f1d6537d775",
+    "url": "/backingSystemDashbord/index.html"
+  },
+  {
+    "revision": "fe36aa774a8a064c6d6e",
+    "url": "/backingSystemDashbord/static/css/main.e335ad7f.chunk.css"
+  },
+  {
+    "revision": "8414be898b1c7fd3dd5e",
+    "url": "/backingSystemDashbord/static/js/2.3876fa25.chunk.js"
+  },
+  {
+    "revision": "54ec399522ed23d09904409d431a135d",
+    "url": "/backingSystemDashbord/static/js/2.3876fa25.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "94909ba437cba250e720",
+    "url": "/backingSystemDashbord/static/js/3.ae40edff.chunk.js"
+  },
+  {
+    "revision": "fe36aa774a8a064c6d6e",
+    "url": "/backingSystemDashbord/static/js/main.2e955168.chunk.js"
+  },
+  {
+    "revision": "e0503200ab04ab53f6ba",
+    "url": "/backingSystemDashbord/static/js/runtime-main.47dd0cd1.js"
+  },
+  {
+    "revision": "1931427c23716bb4ff98d03627f7137c",
+    "url": "/backingSystemDashbord/static/media/fa-brands-400.1931427c.ttf"
+  },
+  {
+    "revision": "b6356c957274676e6571c1ff5e11c9a8",
+    "url": "/backingSystemDashbord/static/media/fa-brands-400.b6356c95.woff2"
+  },
+  {
+    "revision": "023a4a925fa3fce0f66b769ef6bbb264",
+    "url": "/backingSystemDashbord/static/media/fa-regular-400.023a4a92.woff2"
+  },
+  {
+    "revision": "a0cc1c8265e3163aa654a5284ea11ace",
+    "url": "/backingSystemDashbord/static/media/fa-regular-400.a0cc1c82.ttf"
+  },
+  {
+    "revision": "25914cfeafced317e1a0372187fbb2b9",
+    "url": "/backingSystemDashbord/static/media/fa-solid-900.25914cfe.ttf"
+  },
+  {
+    "revision": "6c4eee562650e53cee32496bdfbe534b",
+    "url": "/backingSystemDashbord/static/media/fa-solid-900.6c4eee56.woff2"
+  },
+  {
+    "revision": "71808e147dc6d82c198a4ba292c0cb69",
+    "url": "/backingSystemDashbord/static/media/fa-v4compatibility.71808e14.ttf"
+  },
+  {
+    "revision": "c9e50ccabe9e36f370272197595ea1e5",
+    "url": "/backingSystemDashbord/static/media/fa-v4compatibility.c9e50cca.woff2"
+  },
+  {
+    "revision": "38983b77441c4a31a84e2a3d1eefe91c",
+    "url": "/backingSystemDashbord/static/media/loginbackground.38983b77.jpg"
+  }
+]);
